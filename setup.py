962f2535d9bab61016ff46e30e01fc9033821791
Jonas Nelle <jonas.a.nelle@gmail.com>
2023-10-24 11:44:23 -0400

    Fix package (#14)
    * add project description and allow local autotab chrome extension path
    
    * merge, fix package, format

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="autotab",
+    description="Build browser agents for real world tasks.",
     version="0.1.0",
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    py_modules=["autotab"],
+    package_dir={"": "src"},
     entry_points={
         "console_scripts": [
             "autotab = main:main",
