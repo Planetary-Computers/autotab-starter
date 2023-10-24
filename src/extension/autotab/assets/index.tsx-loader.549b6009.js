b25b5e45ab1ea758f164f77f234cf52923e2ff57
Jonas Nelle <jonas.a.nelle@gmail.com>
2023-10-24 16:55:46 -0400

    Move to using api key for autotab auth (#15)
    * move to using api key for autotab auth
    
    * rebuild extension
    
    * fix open sidepanel and rebuild  extension

--- src/extension/autotab/assets/index.tsx-loader.549b6009.js
+++ src/extension/autotab/assets/index.tsx-loader.549b6009.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-5c235a21.js")
+      chrome.runtime.getURL("assets/chunk-94ceec63.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
