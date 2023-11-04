e0f8bc4c4843d8fe2590d240d2807c3f35c2a522
Jonas Nelle <jonas.a.nelle@gmail.com>
2023-11-04 00:23:13 -0400

    add new dep for dropdowns (#27)

--- src/template.py
+++ src/template.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.action_chains import ActionChains  # noqa: F401
 from selenium.webdriver.common.by import By  # noqa: F401
 from selenium.webdriver.common.keys import Keys  # noqa: F401
 from selenium.webdriver.support import expected_conditions as EC  # noqa: F401
+from selenium.webdriver.support.ui import Select  # noqa: F401
 from selenium.webdriver.support.ui import WebDriverWait  # noqa: F401
 
 from utils.auth import google_login, login  # noqa: F401
